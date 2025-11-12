16b6aeff42a2ddd62e028f87f745ad4e921af8cf
maria <mamani.herrera6@gmail.com>
2025-11-11 20:58:28 -0500

    Primer commit del proyecto

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace MiPrimerPrograma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("¿Cómo te llamas?");
+            string nombre = Console.ReadLine();
+            Console.WriteLine("¡Hola, " + nombre + "!");
+
+            
+            DateTime fecha = DateTime.Now;
+            Console.WriteLine("Hoy es " + fecha.ToString("dd/MM/yyyy"));
+
+            Console.WriteLine("Ingrese número 1:");
+            string a = Console.ReadLine();
+
+            Console.WriteLine("Ingrese número 2:");
+            string b = Console.ReadLine();
+
+            int suma = Convert.ToInt32(a) + Convert.ToInt32(b);
+            Console.WriteLine("El resultado de la suma es: " + suma);
+        }
+    }
+}
