9f4eed3f4a10626ecc3a8d67bbad712887d01b3d
maria <mamani.herrera6@gmail.com>
2025-11-11 22:10:22 -0500

    tercer commit

---
+++ Arrays.cs
@@ -0,0 +1,28 @@
+string[] frutas = new string[3];
+frutas[0] = "Manzana";
+frutas[1] = "Banana";
+frutas[2] = "Cereza";
+
+Console.WriteLine("Las frutas son:");
+foreach (string fruta in frutas)
+{
+    Console.WriteLine(fruta);
+}
+
+List<string> verduras = new List<string> { "Lechuga", "Tomate", "Zanahoria" };
+Console.WriteLine("Las verduras son:");
+foreach (string verdura in verduras)
+{
+    Console.WriteLine(verdura);
+}
+
+
+Dictionary<int, string> frutasdic = new Dictionary<int, string>();
+frutasdic.Add(1, "Manzana");
+frutasdic.Add(2, "Banana");
+frutasdic.Add(3, "Cereza");
+
+foreach (KeyValuePair<int, string> par in frutasdic)
+{
+    Console.WriteLine("Clave: " + par.Key + ", Valor: " + par.Value);
+}
